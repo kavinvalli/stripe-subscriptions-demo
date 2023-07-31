80e987e35d7994adc9640536290d306ca05ffcb1
Kavin Valli <mail@kavin.me>
2023-07-31 18:22:22 +0530

    add logo to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="h-[calc(100vh-57px)] flex justify-center items-center">
+    <main className="h-[calc(100vh-57px)] flex gap-4 justify-center items-center">
+      <Image width={100} height={100} src="/logo.png" alt="" />
       <h1 className="text-6xl font-bold">Subscriptions Demo</h1>
     </main>
   );
